378bf9c36d59538aa922c3061969002d4a6dd1c8
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2020-03-07 09:02:56 -0500

    Initialize

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ $(function () {
     var c = 'css/style.css?v=1.1.1';
     var h = 'includes/header.html';
     var f = 'includes/footer.html';
-    var g = '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152375493-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-152375493-1");</script>';
+    var g = '';
+    // var g = '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152375493-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-152375493-1");</script>';
 
     if (window.location.pathname != '/') {
         v = '../' + v;
@@ -30,83 +31,6 @@ $(function () {
             scrollTop: '0px'
         });
     });
-
-
-    // RANDOM NUMBERS
-    function rand(n, l, u) {
-        var lim = n;
-        var nums = [];
-        for (var i = 0; i < lim; i++) {
-            var j = Math.floor(Math.random() * u) + l; // upp/low limits
-            while (nums.indexOf(j) != -1) {
-                j = Math.floor(Math.random() * u) + l;
-            }
-            nums[i] = j;
-        }
-        return nums;
-    }
-
-
-    // MOODBOARD
-    if ($('.moodboard').length > 0) {
-        update_moodboard();
-        $('.moodboard-btn').on('click', function (e) {
-            e.preventDefault();
-            update_moodboard();
-        });
-        function update_moodboard() {
-            $('.moodboard').html('');
-            var api = '584451-c934d8c19cd640e63fc04dd13';
-            var rand_pics = rand(4, 0, pics.length - 1);
-            for (var i = 0; i < rand_pics.length; i++) {
-                var URL = "https://pixabay.com/api/?key=" + api + "&id=" + pics[rand_pics[i]];
-
-                $.getJSON(URL, function (data) {
-                    if (parseInt(data.totalHits) > 0) {
-                        $.each(data.hits, function (i, hit) {
-                            $('.moodboard').append('<div class="board-item"><div class="board-wrapper"><a href="' + hit.pageURL + '" target="_blank" class="moodboard-img" style="background-image:url(' + hit.webformatURL + ');"><span class="hide-SEO">Moodboard Image</span></a></div></div>');
-                            
-                            //console.log(hit.largeImageURL);
-                        });
-                    }
-                });
-            }
-        }
-    }
-
-
-    // SOUNDBOARD
-    if ($('.soundboard').length > 0) {
-        update_soundboard();
-        $('.soundboard-btn').on('click', function (e) {
-            e.preventDefault();
-            update_soundboard();
-        });
-        function update_soundboard() {
-            $('.soundboard').html('');
-            var rand_songs = rand(4, 0, songs.length - 1);
-            for (var i = 0; i < rand_songs.length; i++) {
-                $('.soundboard').append('<div class="board-item"><div class="board-wrapper"><iframe src="https://embed.music.apple.com/us/album/' + songs[rand_songs[i]] + '"></iframe></div></div>');
-            }
-        }
-    }
-    
-    
-    // BOOKBOARD
-    if ($('.bookboard').length > 0) {
-        update_bookboard();
-        $('.bookboard-btn').on('click', function (e) {
-            e.preventDefault();
-            update_bookboard();
-        });
-        function update_bookboard() {
-            $('.bookboard').html('');
-            var rand_books = rand(4, 0, books.length - 1);
-            for (var i = 0; i < rand_books.length; i++) {
-                $('.bookboard').append('<div class="board-item">' + books[rand_books[i]] + '</div>');
-            }
-        }
-    }
     
     
 });

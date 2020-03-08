60eb3765d74ba30b3646946383bb2379832abb2a
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2020-03-07 20:34:02 -0500

    GA, Our Authors Page, Updates

--- js/app.js
+++ js/app.js
@@ -3,13 +3,12 @@ $(function () {
 
     // LOAD INCLUDES
     var m = '<meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="UTF-8">';
-    var s = '<link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text:300|Roboto:300,300i,400&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" crossorigin="anonymous">';
+    var s = '<link href="https://fonts.googleapis.com/css?family=Raleway:500|Roboto:300,400&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" crossorigin="anonymous">';
     var v = 'img/favicon.png';
     var c = 'css/style.css?v=1.1.1';
     var h = 'includes/header.html';
     var f = 'includes/footer.html';
-    var g = '';
-    // var g = '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152375493-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-152375493-1");</script>';
+    var g = '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160020484-1"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-160020484-1");</script>';
 
     if (window.location.pathname != '/') {
         v = '../' + v;

ee741dc94071b75344b91a35f2f90c42a421828f
writerhaloscot <57688841+writerhaloscot@users.noreply.github.com>
2020-11-22 21:10:57 -0500

    awards update

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function () {
     var m = '<meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1"><meta charset="UTF-8"><meta itemprop="name" content="Queer Indie"><meta itemprop="description" content="Queer Indie is a group of cross-genre authors writing LGBTQ books. Every book has a home. Write the rainbow. This is YOUR story. Be your own identity.">';
     var s = '<link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text:400|Roboto:400,700&display=swap" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" crossorigin="anonymous">';
     var v = 'img/favicon.png';
-    var c = 'css/style.css?v=1.1.4';
+    var c = 'css/style.css?v=1.1.5';
     var h = 'includes/header.html';
     var f = 'includes/footer.html';
     var l = 'includes/lightbox.html';
